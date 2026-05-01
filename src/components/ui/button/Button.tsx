2a73bf04078f9177af88375b7506966307fc8890
riyanfazrirahman <riyanfazrirahman@gmail.com>
2026-05-01 22:32:34 +0700

    fix btn

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -31,7 +31,7 @@ const Button: React.FC<ButtonProps> = ({
   // Variant Classes
   const variantClasses = {
     primary:
-      "bg-gradient-to-br from-blue-500 to-blue-600 text-white shadow-xl hover:shadow-2xl hover:shadow-blue-500/50 hover:scale-[1.02] active:scale-[0.98] disabled:from-blue-300 disabled:to-blue-400 disabled:shadow-none",
+      "bg-gradient-to-br from-blue-500 to-blue-600 text-white shadow-xl hover:shadow-2xl hover:shadow-blue-500/50 active:scale-[0.98] disabled:from-blue-300 disabled:to-blue-400 disabled:shadow-none",
     outline:
       "bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03] dark:hover:text-gray-300",
   };
